e31e9e175ae797612b4dcb1b68c5a5fe6c55751f
Jean-Francois Monin <jean-francois.monin@univ-grenoble-alpes.fr>
2026-06-13 10:21:32 +0200

    Examples/remap2.v in _RocqProject

--- Examples/remap2_handcrafted.v
+++ Examples/remap2_handcrafted.v
@@ -14,28 +14,28 @@ Notation "[ x ]" := (cons x nil).
 Notation "[ x ; y ; .. ; z ]" :=  (cons x (cons y .. (cons z nil) ..)).
 
 (* Partial inductive types *)
-Variant vect_O (A : Set) : Set :=  nil_O : vect_O A.
-Variant vect_S (A : Set) (n : nat) : Set :=  cons_S : A → vect A n → vect_S A n.
-Arguments nil_O {A}.
-Arguments cons_S {A n} _.
+Variant vect_O (X : Type) : Type :=  nil_O : vect_O X.
+Variant vect_S (X : Type) (n : nat) : Type :=  cons_S : X → vect X n → vect_S X n.
+Arguments nil_O {X}.
+Arguments cons_S {X n} _.
 
-Definition sinv_vect_type A n : Set :=
-  match n with  O => vect_O A  |  S n => vect_S A n  end.
+Definition sinv_vect_type X n : Type :=
+  match n with  O => vect_O X  |  S n => vect_S X n  end.
 
-Definition sinv_vect {A n} (u : vect A n) : sinv_vect_type A n :=
+Definition sinv_vect {X n} (u : vect X n) : sinv_vect_type X n :=
   match u with  nil => nil_O  |  cons x u => cons_S x u  end.
 
-Variant vect_O_dep A : vect A 0 -> Set :=
-  nil_O_dep : vect_O_dep A nil.
-Variant vect_S_dep A n : vect A (S n) -> Set :=
-  cons_S_dep x u : vect_S_dep A n (cons x u).
-Arguments nil_O_dep {A}.
-Arguments cons_S_dep {A n} _.
+Variant vect_O_dep X : vect X 0 -> Type :=
+  nil_O_dep : vect_O_dep X nil.
+Variant vect_S_dep X n : vect X (S n) -> Type :=
+  cons_S_dep x u : vect_S_dep X n (cons x u).
+Arguments nil_O_dep {X}.
+Arguments cons_S_dep {X n} _.
 
-Definition sdinv_vect_type A n : vect A n -> Set :=
-  match n with  O => vect_O_dep A  |  S n => vect_S_dep A n  end.
+Definition sdinv_vect_type X n : vect X n -> Type :=
+  match n with  O => vect_O_dep X  |  S n => vect_S_dep X n  end.
 
-Definition sdinv_vect {A n} (u : vect A n) : sdinv_vect_type A n u :=
+Definition sdinv_vect {X n} (u : vect X n) : sdinv_vect_type X n u :=
   match u with  nil => nil_O_dep  |  cons x u => cons_S_dep x u  end.
 
 
@@ -46,7 +46,7 @@ Notation "'let_cons' ( A ,  B ) := E 'in' F" :=
 (* ====================================================================== *)
 (* Basic map2 function *)
 
-Fixpoint map2 {A B C : Set} (f : A → B → C) {n} (u : vect A n) :
+Fixpoint map2 {A B C : Type} (f : A → B → C) {n} (u : vect A n) :
   vect B n → vect C n :=
   match u with
   | []     => λ v, []
@@ -54,13 +54,13 @@ Fixpoint map2 {A B C : Set} (f : A → B → C) {n} (u : vect A n) :
   end.
 
 (* A map2 function on vectors that remembers its inputs in its type *)
-Inductive Remap2 {C A B : Set} : ∀ {n}, vect A n → vect B n → Set :=
+Inductive Remap2 {C A B : Type} : ∀ {n}, vect A n → vect B n → Type :=
 | Rmnil : Remap2 [] []
 | Rmcons {a b n} {aa : vect A n} {bb : vect B n} :
   C → Remap2 aa bb → Remap2 (a :: aa) (b :: bb).
 Arguments Remap2 C {A B n} _ _.
 
-Fixpoint remap2 {A B C : Set} (f : A → B → C) {n} (u : vect A n) :
+Fixpoint remap2 {A B C : Type} (f : A → B → C) {n} (u : vect A n) :
   ∀ v : vect B n, Remap2 C u v :=
   match u with
   | []     => λ v, let_nil () := sdinv_vect v in Rmnil
