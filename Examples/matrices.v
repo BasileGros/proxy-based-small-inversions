8c9bc9422ca15a1ce9537c2c5d6bb79f7c1df320
Jean-Francois Monin <jean-francois.monin@univ-grenoble-alpes.fr>
2026-06-22 10:10:03 +0200

    Examples/matrices.v

--- Examples/matrices.v
+++ Examples/matrices.v
@@ -9,30 +9,6 @@ From Examples Require Import examples_header.
 (* In order to allow ∀ and λ notations *)
 From Stdlib Require Import Utf8.
 
-
-
-Inductive vect (A : Type) : nat -> Type :=
-| nil : vect A 0
-| cons : A -> forall n : nat, vect A n -> vect A (S n).
-
-Unset Elimination Schemes (* For comfort *).
-
-Derive InvProxy for vect.
-(* vect_O vect_S *)
-Derive Dependent InvProxy for vect.
-(* vect_O_dep vect_S_dep *)
-Set Elimination Schemes.
-
-
-
-Arguments cons {A} _ {n}.
-Arguments nil {A}.
-
-Notation "[ ]" := nil (format "[ ]").
-Notation "x :: v" := (cons x v).
-Notation "[ x ]" := (cons x nil).
-Notation "[ x ; y ; .. ; z ]" :=  (cons x (cons y .. (cons z nil) ..)).
-
 (* ------------------------------------------ *)
 (* Renaming finite sets into bounded nat : *)
 
@@ -47,264 +23,252 @@ Derive Dependent InvProxy for bn.
 (* bn_O_dep bn_S_dep *)
 
 
-(* ------------------------------------------ *)
+(* cons with n as first index is more convenient *)
+Inductive vect (A : Type) : nat -> Type :=
+| nil : vect A 0
+| cons : ∀ n, A → vect A n → vect A (S n).
+
+Unset Elimination Schemes (* For comfort *).
+
+Derive InvProxy for vect.
+(* vect_O vect_S *)
+Derive Dependent InvProxy for vect.
+(* vect_O_dep vect_S_dep *)
+Set Elimination Schemes.
+
+Arguments cons {A} {n} _.
+Arguments nil {A}.
+
+Notation "[ ]" := nil (format "[ ]").
+Notation "x :: v" := (cons x v).
+Notation "[ x ]" := (cons x nil).
+Notation "[ x ; y ; .. ; z ]" :=  (cons x (cons y .. (cons z nil) ..)).
 
 (* For convenience *)
 Notation proxy_vectS u := (invproxy u : vect_S _ _).
 
-Definition atVect {A n} (u : vect A n) : bn n → A :=
-  λ i,
-  (fix loop {n} (i : bn n) {struct i} :=
-  match i in bn n return vect A n → A with
-  | BO    => λ u, let (x, _) := proxy_vectS u in x
-  | BS i' => λ u, let (_, u') := proxy_vectS u in loop i' u'
-  end) n i u.
+(* ====================================================================== *)
+(* Functional implementation of vectors *)
 
-Definition mkVect {A} : ∀ {n}, (bn n → A) → vect A n :=
+Definition fvect (A : Type) : nat -> Type := λ n, bn n → A.
+
+(* From vectors to functional vectors... *)
+Fixpoint ith {A n} (u : vect A n) : fvect A n :=
+  match u with
+  | []      => λ i, match invproxy i : bn_O with end
+  | x :: u' => λ i, match invproxy i with
+                    | BO_S _    => x
+                    | BS_S _ i' => ith u' i'
+                    end
+  end.
+
+Tactic Notation "simpl_ith" :=
+  change (ith (?x :: ?u) BO) with x ||
+  change (ith (?x :: ?u) (BS ?i')) with (ith u i').
+
+Tactic Notation "simpl_ith_in" hyp(H) :=
+  change (ith (?x :: ?u) BO) with x in H ||
+  change (ith (?x :: ?u) (BS ?i')) with (ith u i') in H.
+
+(* ... and conversely *)
+Definition mkVect {A} : ∀ {n}, fvect A n → vect A n :=
   fix loop n :=
     match n with
-    | O => λ f, []
-    | S n' => λ f, f BO :: loop n' (λ i, f (BS i))
+    | O    => λ f, []
+    | S n' => λ f, f BO :: loop n' (f ∘ BS)
     end.
 
-Lemma atVect_mkVect {A n} (f : bn n → A) : ∀ i, f i = atVect (mkVect f) i.
+Ltac simpl_mkVect :=
+  change (mkVect ?f) with [] ||
+  change (mkVect ?f) with (f BO :: mkVect (f ∘ BS)).
+
+(* [ith] is extensionally the left inverse of [mkVect] *)
+Lemma ith_mkVect {A n} (fv : fvect A n) : ∀ i, fv i = ith (mkVect fv) i.
 Proof.
   induction n as [ | n Hn]; intro i.
   - sinv i.
-  - sdinv i as [ | i'].
-    + cbn. reflexivity.
-    + cbn. exact (Hn _ i').
+  - sdinv i as [ | i']; simpl_mkVect; simpl_ith.
+    + reflexivity.
+    + exact (Hn _ i').
 Qed.
 
-Lemma atVect_extensional {A n} (u v : vect A n) :
-  (∀ i, atVect u i = atVect v i) → u = v.
+(* Then [ith] is extensional *)
+Lemma ith_extensional {A n} (u v : vect A n) :
+  (∀ i, ith u i = ith v i) → u = v.
 Proof.
-  revert n u v.
-  fix loop 2. intros n u v e.
-  destruct u as [ | x n' u'].
+  revert v; induction u as [ | x n u' Hu']; intros v e.
   - sdinv v. reflexivity.
   - sdinv v as [y v'].
-    generalize (e BO). cbn. intro e1. case e1. f_equal.
-    apply (loop n' u' v'). intro i.
-    generalize (e (BS i)). simpl (* better than cbn *).
-    intro e'; exact e'.
+    generalize (e BO). cbn. intro e1. f_equal; [ exact e1 | ].
+    apply (Hu' v'); intro i. specialize (e (BS i)). simpl_ith_in e.
+    exact e.
 Qed.
 
-Corollary mkVect_atVect {A n} (v : vect A n) : v = mkVect (atVect v).
+(* Then mkVect is the left inverse of ith *)
+Corollary mkVect_ith {A n} (v : vect A n) : v = mkVect (ith v).
 Proof.
-  apply atVect_extensional; intro i. apply atVect_mkVect.
-Qed.
-
-Lemma mkVect_atVect_direct {A n} (v : vect A n) : v = mkVect (atVect v).
-Proof.
-  induction v as [ | x n v Hv].
-  - cbn. reflexivity.
-  - cbn [mkVect]. f_equal. simpl.
-    apply Hv.
-Qed.
-
-
-(* Alternative definition, avoiding the use of simpl above *)
-Fixpoint atVectV {A n} (u : vect A n) {struct u} :  bn n → A :=
-  match u with
-  | [] => λ i, match invproxy i in bn_O with end (* ballot : faut préciser SP_bn_O *)
-  | x :: u' => λ i,
-      match invproxy i with
-      | BO_S _    => x
-      | BS_S _ i' => atVectV u' i'
-      end
-  end.
-(*
-  refine
-      match proxy i in SP_bn_S _ with
-      | SP_BO_S => x
-      | SP_BS_S i' => atVectV _ _ u' i' (* BUG AI *)
-      end
-  .
- *)
-
-Lemma atVectV_mkVect {A n} (f : bn n → A) : ∀ i, f i = atVectV (mkVect f) i.
-Proof.
-  induction n as [ | n Hn]; intro i.
-  - sinv i.
-  - sdinv i as [ | i'].
-    + cbn. reflexivity.
-    + cbn.
-      exact (Hn _ i').
-Qed.
-
-Lemma mkVect_atVectV {A n} (v : vect A n) : v = mkVect (atVectV v).
-Proof.
-  induction v as [ | x n v Hv]; cbn.
-  - reflexivity.
-  - f_equal. apply Hv.
-Qed.
-
-Lemma atVectV_extensional {A n} (u v : vect A n) : (∀ i, atVectV u i = atVectV v i) → u = v.
-Proof.
-  revert n u v. fix loop 2. intros n u v e.
-  destruct u as [ | x n' u'].
-  - sdinv v. reflexivity.
-  - sdinv v as [y v'].
-    generalize (e BO). cbn. intro e1. case e1. f_equal.
-    apply (loop n' u' v'). intro i.
-    generalize (e (BS i)). cbn.
-    intro e'; exact e'.
+  apply ith_extensional; intro i. apply ith_mkVect.
 Qed.
 
 (* ====================================================================== *)
+
+Definition vect_cst {A} (a : A) {n} : vect A n := mkVect (λ i, a).
+
+(* Equivalent definition that produces more readable goals *)
+Definition map0 {A} (a : A) : ∀ n, vect A n:=
+  fix loop n :=  match n with  O => [] | S n' => a :: loop n' end.
+
+Definition map2 {A B C} (f : A → B → C) :
+  ∀ {n}, vect A n → vect B n → vect C n :=
+  fix loop _ (u : vect A _) :=
+    match u with
+    | []      => λ v, []
+    | x :: u' => λ v,
+        let (y, v') := proxy_vectS v in
+        f x y :: loop _ u' v'
+    end.
+
+Ltac simpl_map2 :=
+  change (map2 ?op (?x :: ?u) (?y :: ?v)) with (op x y :: map2 op u v).
+
+(* ====================================================================== *)
 (* Matrices *)
-
-(*
-     | cW |  matE      |
-  ---+----+------------+
-  lN | NW |    NE      |
-  ---+----+------------+
-     |    |            |
-     |    |            |
-matS | SW |    SE      |
-     |    |            |
-     |    |            |
-  ---+----+------------+
-
- *)
 
 (* Matrix (m, n) : m lines of n elements (n columns) *)
 Definition matrix (A : Type) : nat → nat → Type :=
   λ m n, vect (vect A n) m.
 
-Definition atMat {A m n} (mat : matrix A m n) (i : bn m) (j : bn n) : A :=
-  atVect (atVect mat i) j.
+(* ====================================================================== *)
+(* Functional representation of matrices *)
 
+Definition fmatrix (A : Type) (m n : nat) :=
+  bn m → bn n → A.
+
+(* From matrices to functional matrices... *)
+Definition ijth {A m n} (mat : matrix A m n) : fmatrix A m n :=
+  λ i j, ith (ith mat i) j.
+Ltac fold_ijth := change (ith (ith ?mat ?i) ?j) with (ijth mat i j).
+Ltac simpl_ijth :=
+  change (ijth (?a :: ?b) BO ?j) with (ith a j) ||
+  change (ijth (?a :: ?b) (BS ?i) ?j) with (ijth b i j).
+
+(* ... and conversely *)
 Definition mkMat {A m n} (f : bn m → bn n → A) : matrix A m n :=
   mkVect (λ i, mkVect (f i)).
 
-Lemma atMat_mkMat {A m n} (f : bn m → bn n → A) : ∀ i j, f i j = atMat (mkMat f) i j.
+(* [ijth] is extensionally the left inverse of [mkMat] *)
+Lemma ijth_mkMat {A m n} (f : fmatrix A m n) :
+  ∀ i j, f i j = ijth (mkMat f) i j.
 Proof.
-  intros i j.
-  unfold atMat, mkMat.
-  case atVect_mkVect.
-  case atVect_mkVect.
-  reflexivity.
+  intros i j. unfold ijth, mkMat.
+  do 2 case ith_mkVect. reflexivity.
 Qed.
 
-Lemma atMat_extensional {A m n} (mat mat' : matrix A m n) :
-  (∀ i j, atMat mat i j = atMat mat' i j) → mat = mat'.
+(* Then [ijth] is extensional *)
+Lemma ijth_extensional {A m n} (mat mat' : matrix A m n) :
+  (∀ i j, ijth mat i j = ijth mat' i j) → mat = mat'.
 Proof.
-  intros e.
-  apply atVect_extensional; intro i.
-  apply atVect_extensional; intro j.
-  apply e.
+  intro e. do 2 (apply ith_extensional; intro). fold_ijth. apply e.
 Qed.
 
-Corollary mkMat_atMat {A m n} (mat : matrix A m n) : mat = mkMat (atMat mat).
+(* Then mkMat is the left inverse of ijth *)
+Corollary mkMat_ijth {A m n} (mat : matrix A m n) :
+  mat = mkMat (ijth mat).
+Proof. apply ijth_extensional, ijth_mkMat. Qed.
+
+(* ====================================================================== *)
+
+(* Transposition on a functional matrix, easy but quadratic complexity *)
+
+Definition swap {X Y Z} (f : X → Y → Z) : Y → X → Z := λ x y, f y x.
+
+Definition ftranspose {A m n} : matrix A m n → matrix A n m :=
+  λ mat, mkMat (swap (ijth mat)).
+
+Compute ftranspose [[1; 2]; [3; 4]; [5; 6]].
+
+Lemma ftranspose_corr {A m n} (mat : matrix A m n) :
+  ∀ j i, ijth mat i j = ijth (ftranspose mat) j i.
+Proof. exact (ijth_mkMat (swap (ijth mat))). Qed.
+
+Corollary ftranspose_invol {A m n} (mat : matrix A m n) :
+  mat = ftranspose (ftranspose mat).
 Proof.
-  apply atMat_extensional; intros i j. apply atMat_mkMat.
-Qed.
-
-
-Definition swap {A B C} (f : A → B → C) : B → A → C := λ a b, f b a.
-
-Definition swapMat {A m n} : matrix A m n → matrix A n m :=
-  λ mat, mkMat (swap (atMat mat)).
-
-Compute swapMat [[1; 2]; [3; 4]; [5; 6]].
-
-Lemma swapMat_corr {A m n} (mat : matrix A m n) :
-  ∀ j i, atMat mat i j = atMat (swapMat mat) j i.
-Proof. exact (atMat_mkMat (swap (atMat mat))). Qed.
-
-Corollary swapMat_invol {A m n} (mat : matrix A m n) :
-  mat = swapMat (swapMat mat).
-Proof.
-  apply atMat_extensional; intros i j.
-  case swapMat_corr.
-  apply swapMat_corr.
+  apply ijth_extensional; intros i j.
+  case ftranspose_corr. apply ftranspose_corr.
 Qed.
 
 (* ---------------------------------------------------------------------- *)
+(* Transposition on the implementation using vectors, linear complexity *)
 
-Definition transpose_mn {A} : ∀ {m n}, matrix A m n → matrix A n m :=
-  fix loopm m :=
-    match m return ∀ n, matrix A m n → matrix A n m with
-    | O    => λ n _, mkVect (λ _, [])
-    | S m' => λ n mat,
-        let (lN, matS) := proxy_vectS mat in
-        let matS_ := loopm m' n matS in
-        (fix loopn n  :=
-           match n return
-                 vect A n → matrix A n m' → matrix A n (S m') with
-           | O    => λ lN matS_, []
-           | S n' => λ lN matS_,
-               let (NW, NE)   := proxy_vectS lN in
-               let (SW_, SE_) := proxy_vectS matS_ in
-               (NW :: SW_) :: loopn n' NE SE_
-           end
-        ) n lN matS_
-    end.
-
-Compute transpose_mn [[1; 2]; [3; 4]; [5; 6]].
-
-(* -------------------------------------------------- *)
-
-Definition cons_column {A n} :
-  ∀ {m} (u : vect A m), matrix A m n → matrix A m (S n) :=
-  fix loop {m} u {struct u} :=
-    match u in vect _ m return matrix A m n → matrix A m (S n) with
-    | []      => λ mat, []
-    | x :: u' => λ mat,
-        let (line, mat') := proxy_vectS mat in
-        (x :: line) :: loop u' mat'
-    end.
-
-Definition transpose {A} : ∀ {m n}, matrix A m n → matrix A n m :=
-  fix loop {m n} (mat : matrix A m n) {struct mat} : matrix A n m :=
-    match mat in vect _ m return matrix A n m with
-    | []       => mkVect (λ _, [])
-    | lN :: matS => cons_column lN (loop matS)
-    end.
+Fixpoint transpose {A m n} (mat : matrix A m n) : matrix A n m :=
+  match mat with
+  | []        => map0 [] n
+  | v :: mat' => map2 cons v (transpose mat')
+  end.
+Ltac simpl_transpose :=
+  change (transpose (?v :: ?mat')) with (map2 cons v (transpose mat')).
 
 Compute transpose [[1; 2]; [3; 4]; [5; 6]].
 
-Definition at_cons_column {A m n} (u : vect A m) (mat : matrix A m n) :
-  bn m → bn (S n) → A := λ i j,
+(* The following definition is for information (map2 cons will
+   be directly used instead). However, in order to prove
+   properties of transpose, a good strategy is to start with
+   a corresponding lemma on cons_column *)
+Definition cons_column {A n m} :
+  ∀ (u : vect A m), matrix A m n → matrix A m (S n) :=
+  map2 cons.
+
+(* [u] represents a column on the left of [mat] *)
+Definition ijth_colmat {A m n} (u : vect A m) (mat : matrix A m n) :
+  fmatrix A m (S n) := λ i j,
     match j with
-    | BO   => λ _, atVect u i
-    | BS j' => λ mat, atMat mat i j'
+    | BO    => λ  _ , ith u i
+    | BS j' => λ mat, ijth mat i j'
     end mat.
 
-Lemma cons_column_corr {A m n} (u : vect A m) (mat : matrix A m n) :
-  ∀ i j, at_cons_column u mat i j = atMat (cons_column u mat) i j.
+(* Correctness lemma on ijth_colmat *)
+Lemma ijth_colmat_corr {A m n} (u : vect A m) (mat : matrix A m n) :
+  ∀ i j, ijth_colmat u mat i j = ijth (map2 cons u mat) i j.
 Proof.
-  revert m u mat. fix loop 2. intros m u mat.
-  destruct u as [ | x m' u']; intros i j.
+  induction u as [ | x m u Hu]; intros i j.
   - sinv i.
-  - sdinv mat as [line mat'].
+  - sdinv mat as [line mat']. simpl_map2.
     sdinv i as [ | i'].
-    + sdinv j as [ | j']; reflexivity.
-    + simpl.
-      change (atMat (?a :: ?b) (BS ?i) ?j) with (atMat b i j).
-      generalize (loop m' u' mat' i' j).
-      sdinv j as [ | j']; intro e; exact e.
+    + cbv [ijth]. sdinv j as [ | j'].
+      all: cbv [ijth_colmat ijth]. all: repeat simpl_ith. all: reflexivity.
+    + simpl_ijth. case (Hu mat'). sdinv j as [ | j'].
+      all: cbv [ijth_colmat ijth]. all: simpl_ith. all: reflexivity.
 Qed.
 
+(* Correctness lemma on transpose *)
 Lemma transpose_corr {A m n} (mat : matrix A m n) :
-  ∀ i j, atMat mat i j = atMat (transpose mat) j i.
+  ∀ i j, ijth mat i j = ijth (transpose mat) j i.
 Proof.
-  revert n m mat. fix loop 3. intros n m mat.
-  destruct mat as [ | lN m' matS]; intros i j.
+  induction mat as [ | v m' mat' Hmat']; intros i j.
   - sinv i.
-  - cbn [transpose].
-    case cons_column_corr.
-    sdinv i as [ | i'].
+  - cbn [transpose]. case ijth_colmat_corr.
+    sdinv i as [ | i']; cbn; simpl_ijth.
     + reflexivity.
-    + apply (loop _ _ matS).
+    + apply Hmat'.
 Qed.
 
-Corollary transpose_swap {A m n} (mat : matrix A m n) : swapMat mat = transpose mat.
+(* Another correctness lemma on transpose, based on ftranspose
+   seen as a functional specification of transpose *)
+Corollary transpose_fctspec {A m n} (mat : matrix A m n) :
+  ftranspose mat = transpose mat.
 Proof.
-  apply atMat_extensional; intros i j. case swapMat_corr. apply transpose_corr.
+  apply ijth_extensional; intros i j. case ftranspose_corr. apply transpose_corr.
 Qed.
 
-Corollary transpose_invol {A m n} (mat : matrix A m n) : mat = transpose (transpose mat).
-Proof. do 2 case transpose_swap. apply swapMat_invol. Qed.
+(* Then, transpose is involutive... *)
+Corollary transpose_invol {A m n} (mat : matrix A m n) :
+  mat = transpose (transpose mat).
+Proof. do 2 case transpose_fctspec. apply ftranspose_invol. Qed.
+
+(* ... and injective *)
+Corollary transpose_injective {A m n} (ma mb : matrix A m n) :
+  transpose ma = transpose mb → ma = mb.
+Proof.
+  intro e. rewrite (transpose_invol mb). case e. apply transpose_invol.
+Qed.
