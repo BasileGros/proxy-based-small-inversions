2bcf431c1da68fb66520729499754fed597a5cdd
Basile <gros.basile@free.fr>
2026-07-31 09:10:23 +0200

    Corrected example import

--- Examples/examples_header.v
+++ Examples/examples_header.v
@@ -1,8 +1,3 @@
-From MetaRocq.Utils Require Export utils.
-From MetaRocq.Template Require Export All.
-From MetaRocq.Template Require Export Checker.
-
-
 From utils Require Export utils.
 From SmallInversion Require Export small_inversion.
 Export Utf8.
